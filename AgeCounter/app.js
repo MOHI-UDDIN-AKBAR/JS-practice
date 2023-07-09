1f4707a81b55ed9bbb3af9e67b358f8f183f3bea
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2023-07-09 16:01:31 +0200

    feat(project-16): Age Counter

--- AgeCounter/app.js
+++ AgeCounter/app.js
@@ -9,8 +9,26 @@ const button = document.querySelector(".age-counter .date button ");
 const handleEvent = () => {
   const date = input.value;
   const getYear = new Date().getFullYear() - new Date(date).getFullYear();
-  age.textContent = getYear;
-  result.style.visibility = "visible";
+  const getMonth = new Date().getMonth() - new Date(date).getMonth();
+  const getDay = new Date().getDate() - new Date(date).getDate();
+  console.log(getYear + "\t" + getMonth + "\t" + getDay);
+  if (getYear > 0) {
+    age.textContent = getYear > 1 ? `${getYear} years` : `${getYear} year`;
+    result.style.visibility = "visible";
+    result.classList.add("success");
+  } else if (getMonth > 0) {
+    age.textContent = getMonth > 1 ? `${getMonth} months` : `${getMonth} month`;
+    result.style.visibility = "visible";
+    result.classList.add("success");
+  } else if (getDay > 0) {
+    age.textContent = getDay > 1 ? `${getDay} days` : `${getDay} day`;
+    result.style.visibility = "visible";
+    result.classList.add("success");
+  } else {
+    result.textContent = "Sorry ! Your Input is not valid ";
+    result.style.visibility = "visible";
+    result.classList.add("danger");
+  }
 };
 
 // Event Listener
